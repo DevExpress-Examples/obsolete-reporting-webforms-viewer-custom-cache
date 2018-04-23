87eade10a9b9ace2209593c0ecdd9622fff79f5f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using System.Web.UI;
+using DevExpress.XtraReports.Web;
+// ...
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+        this.ReportViewer1.Report = new XtraReport1();
+    }
+    protected void ReportViewer1_CacheReportDocument(object sender, CacheReportDocumentEventArgs e) {
+        e.Key = Guid.NewGuid().ToString();
+        Page.Session[e.Key] = e.SaveDocumentToMemoryStream();
+    }
+    protected void ReportViewer1_RestoreReportDocumentFromCache(object sender, RestoreReportDocumentFromCacheEventArgs e) {
+        Stream stream = Page.Session[e.Key] as Stream;
+        if (stream != null)
+            e.RestoreDocumentFromStream(stream);
+    }
+}
